af0579d9e63cc86a1faa60605551d15d75845f6e
Marcelo Cabral Ghilardi <mcabral@publi.com.br>
2017-07-28 11:43:50 -0300

    First Version

--- details.md
+++ details.md
@@ -1,26 +1,9 @@
-## vsts-extension-ts-seed-simple ##
+# vsts-extension-ts-seed-simple #
 
-Very simple seed project for developing VSTS extensions using Typescript. Utilizes Typescript, grunt, and tsd. Detailed explanation how to get started can be found at https://cschleiden.wordpress.com/2016/02/24/extending-vsts-setup/.
+Describe your extension here. This description will be shown in the marketplace. You can use *Markdown*.
 
-### Structure ###
+https://github.com/cschleiden/vsts-extension-ts-seed-simple
 
-```
-/scripts            - Typescript code for extension
-/img                - Image assets for extension and description
-/typings            - Typescript typings
-
-details.md          - Description to be shown in marketplace   
-index.html          - Main entry point
-vss-extension.json  - Extension manifest
-```
-
-### Version History ###
-
-```
-0.7.0 - Updated VSS SDK, moved from `typings` to `@types`
-0.6.0 - Updated VSS SDK to M104
-0.1.1 - Automatically increase extension's minor version when packaging.
-```
 
 ### Usage ###
 
@@ -48,11 +31,3 @@ The VSTS framework is setup to initalize the requirejs AMD loader, so just use `
 #### VS Code ####
 
 The included `.vscode` config allows you to open and build the project using [VS Code](https://code.visualstudio.com/).
-
-#### Unit Testing ####
-
-The project is setup for unit testing using `mocha`, `chai`, and the `karma` test runner. A simple example unit test is included in `scripts/logic/messageHelper.tests.ts`. To run tests just execute:
-
-```
-grunt test
-```
---
+++ scripts/app.ts
@@ -0,0 +1,55 @@
+///<reference types="vss-web-extension-sdk" />
+import * as ExtensionContracts from "TFS/WorkItemTracking/ExtensionContracts";
+import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
+import * as WitService from "TFS/WorkItemTracking/Services";
+import * as VSSUtilsCore from "VSS/Utils/Core";
+
+VSS.register(VSS.getContribution().id, function () {
+            return {
+                // Called when the active work item is modified
+                onFieldChanged: function(args) {
+                    //$("#events").append($("<div/>").text("onFieldChanged - " + JSON.stringify(args)));
+                },
+
+                // Called when a new work item is being loaded in the UI
+                onLoaded: function (args) {
+
+                    var _fieldName = VSS.getConfiguration().witInputs["FieldName"] ;
+
+                    if (_fieldName != undefined) {
+                        WitService.WorkItemFormService.getService().then(
+                            (service) => {
+                                service.getFieldValues([_fieldName]).then(
+                                function (value) {
+                                    var textLink = value[_fieldName] ;
+                                    $("#content").append($("<div/>").text("" + textLink));
+                                });
+                            },
+                            this._handleError
+                        );
+                    }
+                },
+
+                // Called when the active work item is being unloaded in the UI
+                onUnloaded: function (args) {
+                    //$("#events").empty();
+                    //$("#events").append($("<div/>").text("onUnloaded - " + JSON.stringify(args)));
+                },
+
+                // Called after the work item has been saved
+                onSaved: function (args) {
+                    //$("#events").append($("<div/>").text("onSaved - " + JSON.stringify(args)));
+                },
+
+                // Called when the work item is reset to its unmodified state (undo)
+                onReset: function (args) {
+                    //$("#events").append($("<div/>").text("onReset - " + JSON.stringify(args)));
+                },
+
+                // Called when the work item has been refreshed from the server
+                onRefreshed: function (args) {
+                    //$("#events").append($("<div/>").text("onRefreshed - " + JSON.stringify(args)));
+                }
+            }
+        });      
+
